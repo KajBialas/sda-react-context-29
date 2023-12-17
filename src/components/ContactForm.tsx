7aaa0674a7661ed9d8fc56d45c8af1de7a3a7ed2
Kaj Białas <kaj.bialas@dev-industry.com>
2023-12-17 11:59:12 +0100

    Zadanie 3

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,41 @@
+import { useState, ChangeEvent, FormEvent  } from "react";
+
+const INIT_STATE = {
+    fullName: '',
+    email: '',
+    message: ''
+}
+const ContactForm = () => {
+    const [formData, setFormData] = useState(INIT_STATE);
+
+    const handleChange = (e: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { value, name } = e.target as HTMLInputElement | HTMLTextAreaElement;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('form', formData);
+        setFormData(INIT_STATE);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input name="fullName" type="text" placeholder="Imię i nazwisko" value={formData.fullName} onChange={handleChange} />
+            </div>
+            <div>
+                <input name="email" type="text" placeholder="Email" value={formData.email} onChange={handleChange} />
+            </div>
+            <div>
+                <textarea name="message" value={formData.message} placeholder="Wiadomość" onChange={handleChange} />
+            </div>
+            <button type="submit">Wyślij</button>
+        </form>
+    )
+}
+
+export default ContactForm;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,14 @@
+import ContactForm from "../components/ContactForm";
 const Contact = () => {
     return (
         <div>
-            Contact
+            Contact:
+            <ContactForm />
         </div>
     )
 }
 
 export default Contact;
+
+// 1. Utwórz formularz kontaktowy zawierający pola: imię i nazwisko, adres email oraz wiadomość.
+// 2. Po wysłaniu formularza dane powinny wyświetlić się w konsoli
