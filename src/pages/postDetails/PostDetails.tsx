da5f661aaa35f2b46070e6bfec8ed9c911674778
Kaj Białas <kaj.bialas@dev-industry.com>
2024-01-13 15:08:42 +0100

    Error handling

--- src/pages/postDetails/PostDetails.tsx
+++ src/pages/postDetails/PostDetails.tsx
@@ -7,12 +7,39 @@ import styles from './postDetails.style.module.css';
 const PostDetails = () => {
     const { id } = useParams();
     const [ postDetails, setPostDetails] = useState<Post | null>(null);
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ error, setError ] = useState<string | null>(null);
 
     useEffect(() => {
-        fetch(`${API_URL}/${id}`)
-            .then(response => response.json())
-            .then(data => setPostDetails(data))
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${API_URL}/${id}`);
+
+                if (!response.ok) {
+                    throw new Error('HTTP error');
+                }
+
+                const data = await response.json();
+                setPostDetails(data);
+
+            } catch (error) {
+                setError('Error');
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchData();
+
     }, [id]);
+
+    if (isLoading) {
+        return (<div>Ładowanie danych:</div>)
+    }
+
+    if (error) {
+        return (<div>Błąd pobierania danych</div>)
+    }
 
     return (<section className={styles.postDetailsContainer}>
             <h1 className={styles.postDetailsTitle}>{postDetails && postDetails.title}</h1>
